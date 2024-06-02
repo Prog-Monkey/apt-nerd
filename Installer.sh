edd6291172bae5fc8a87c3a3135de380d646ffa3
the champion <154099869+Prog-Monkey@users.noreply.github.com>
2024-06-02 12:22:32 -0400

    Update Installer.sh

--- Installer.sh
+++ Installer.sh
@@ -5,6 +5,9 @@ read url
 echo "[-] Download fonts [-]"
 echo $url
 wget $url
-unzip JetBrainsMono.zip -d ~/.fonts
+echo "What is the name of the zip? Example Jetbrains.zip Please Enter!"
+read Name
+unzip $Name -d ~/.fonts
 fc-cache -fv
+rm $Name
 echo "done!"
