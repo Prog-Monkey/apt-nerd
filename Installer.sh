9a8fa9d70f0f71864d7a43a79cafbdb683a3c503
Suganthan Skandan <legoanimals08@gmail.com>
2024-06-02 23:19:38 -0400

    Initial Commit

--- Installer.sh
+++ Installer.sh
@@ -1,11 +1,22 @@
-#/bin/bash
-echo "Input the Github URL please!"
+#!/bin/sh
+
+echo "Input the GitHub URL please!"
 read url
+
 echo "Downloading $url"
-wget $url
+wget "$url"
+
 echo "What is the name of the zip? Example Jetbrains.zip."
 read Name
-unzip $Name -d ~/.fonts
-fc-cache -fv
-rm $Name
-echo "Thanks for using apt-nerd please star this repo on github!"
+
+# Get the directory of the script
+SCRIPTPATH=$(dirname "$0")
+
+if test -f "$SCRIPTPATH/$Name"; then
+  unzip "$SCRIPTPATH/$Name" -d ~/.fonts
+  fc-cache -fv
+  rm "$SCRIPTPATH/$Name"
+  echo "Thanks for using apt-nerd please star this repo on GitHub!"
+else
+  echo "The zip $Name doesn't exist in the script directory. Please Enter the Name Properly!"
+fi
